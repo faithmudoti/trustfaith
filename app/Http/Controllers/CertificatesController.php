08d0b1f023b0a1eecd07194ac79e2f8aad80c41e
faithmudoti <faithmudoti@gmail.com>
2021-01-19 21:12:28 +0200

    admin

---
+++ app/Http/Controllers/CertificatesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Certificates;
+use Illuminate\Http\Request;
+
+class CertificatesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Certificates  $certificates
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Certificates $certificates)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Certificates  $certificates
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Certificates $certificates)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Certificates  $certificates
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Certificates $certificates)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Certificates  $certificates
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Certificates $certificates)
+    {
+        //
+    }
+}
